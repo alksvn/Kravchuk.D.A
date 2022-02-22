c5e4e75a6c098cb6b217ca9fb4cfd261cbabe5a7
alksvn <91622466+alksvn@users.noreply.github.com>
2022-02-22 23:45:56 +0300

    Update Задание 1(пр 1).java

--- Практика 1/Задание 1(пр 1).java
+++ Практика 1/Задание 1(пр 1).java
@@ -1,4 +1,4 @@
-
+//Реализовать программу, получающую на вход в качестве аргумента имя человека и выводящую "Hello" + имя, в противном случае, если параметр не передавался, "Hello world".
 public class HelloName {
 	public static void main(String[] args) {
 		System.out.println("Hello " + (args.length > 0? args[0] : "world"));
