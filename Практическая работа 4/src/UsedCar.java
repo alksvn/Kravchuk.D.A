5efc9c1e166a2257a8764f830e6788bfab479497
alksvn <91622466+alksvn@users.noreply.github.com>
2022-06-01 01:33:46 +0300

    Add files via upload

---
+++ Практическая работа 4/src/UsedCar.java
@@ -0,0 +1,39 @@
+
+public class UsedCar extends Car {
+	private String degreeOfPreservation;//степень—охранности
+	private String FIOowner;//‘»ќвладельца
+	private int mileage;//пробегјвтомобил€
+	
+	public UsedCar(String modelCar, int release, int price, String equipment, String country, String dataOfSale, String FIObuyer, String degreeOfPreservation, String FIOowner, int mileage) {
+		super(modelCar, release, price, equipment, country, dataOfSale,FIObuyer);
+		this.degreeOfPreservation = degreeOfPreservation;
+		this.FIOowner = FIOowner;
+		this.mileage = mileage;
+	}
+	
+	public void setDegreeOfPreservation(String degreeOfPreservation) {
+		this.degreeOfPreservation = degreeOfPreservation;
+	}
+	public String getDegreeOfPreservation() {
+		return degreeOfPreservation;
+	}
+	
+	public void setFIOowner(String FIOowner) {
+		this.FIOowner = FIOowner;
+	}
+	public String getFIOowner() {
+		return FIOowner;
+	}
+	
+	public void setMileage(int mileage) {
+		this.mileage = mileage;
+	}
+	public int getMileage() {
+		return mileage;
+	}
+	
+	public String toString()
+    {
+        return super.toString() + "\nCтепень сохранности - " + degreeOfPreservation + "\n‘»ќ владельца - " + FIOowner + "\nѕробег - " + mileage;
+    }
+}
